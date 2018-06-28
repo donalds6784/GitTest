3102531d8ed386b64433a38989b4c2113acae67e
Kyambadde Ronald <donalds6784@gmail.com>
2018-06-29 01:54:48 +0300

    Running test for using git

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-console.log('hello.!!');
+//==========================================================================
+//A program that calculates and shows the value of 210 (2 to the 10th power).
+//==========================================================================
+//Using While loop
+/*let result = 1;
+let counter = 0;
+while (counter < 10) {
+  result = result * 2;
+  counter = counter + 1;
+}
+console.log(result);
+// → 1024*/
+
+// using for loop
+/*let result = 1;
+for (let counter = 0; counter < 10; counter = counter + 1) {
+  result = result * 2;
+}
+console.log(result);
+// → 1024
+*/
+//===========================================================
+// A program that prompts a user to input a name in the field.
+//============================================================
+/*let yourName;
+do{
+    yourName = prompt('Who are you.?');
+} while (!yourName) {
+    console.log(yourName);
+}*/
+
+//=========================================================
+// A switch program to let you know of what to wear during 
+//=========================================================
+switch (prompt("What is the weather like?")) {
+    case "rainy":
+      console.log("Remember to bring an umbrella.");
+      break;
+    case "sunny":
+      console.log("Dress lightly.");
+    case "cloudy":
+      console.log("Go outside.");
+      break;
+    default:
+      console.log("Unknown weather type!");
+      break;
+  }
--- dir1/app1.js
+++ dir1/app1.js
@@ -1,3 +1,54 @@
-console.log('why me');
+/*let theNumber = Number(prompt("Pick a number"));
+console.log("Your number is the square root of " +
+            theNumber * theNumber);*/
 
-`why would i wanna give you ${100 / 2}% of my $100.??`;
+//`why would i wanna give you ${100 / 2}% of my $100.??`;
+
+/*let theNumber = Number(prompt("Pick a number"));
+	if (!Number.isNaN(theNumber)) {
+console.log("Your number is the square root of " +
+            theNumber * theNumber);
+    } else {
+        console.log("Hey. Why didn't you give me a number?");
+} */
+
+/*let num = Number(prompt("Pick a number"));
+
+if (num < 10) {
+  console.log("Small");
+} else if (num < 100) {
+  console.log("Medium");
+} else {
+  console.log("Large");
+}*/
+
+//=================================================
+//A function to print odd numbers to the console.
+//=================================================
+
+/*function oddNum(number) {
+    for (let i = 1; i <= number; i++) {
+        if (i % 2 != 0) {
+            console.log(i);
+        }
+    }
+}
+oddNum(7);*/
+ 
+//2nd solution:
+/*function oddNumberCheck(input) {
+    for (let i = 1; i <= input; i += 2) {
+        console.log(i);
+    }
+}
+ 
+oddNumberCheck(7);*/
+
+// ==============================================
+//A program to print odd numbers or even numbers
+//===============================================
+for (i =0; i<= 10; i++) {
+	if (i % 2 === 1) {
+	console.log(i);
+}
+} 
